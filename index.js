ba505d2e13a593d0d23bdcd986db11f51ab91e12
ashwini <adpaithane23@gmail.com>
2025-06-21 17:52:02 +0530

    return update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())//body parser
 app.use(express.static("dist"))
 app.use(cors({ origin: "https://practice2-p6t4.onrender.com" }))
 app.use("/api/employee", require("./routes/employee.routes"))
-mongoose.connect(import.meta.env.VITE_URL)//database la connect karaychi command
+mongoose.connect(process.env.MONGO_URL)//database la connect karaychi command
 mongoose.connection.once("open", () => {
     console.log("db connected");
     app.listen(process.env.PORT, console.log("server running"));// server open caraychi command nodemon takych
